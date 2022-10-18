bbdfd54651b1fea91f8a871fe02b0ca89dd04163
Luke Lenhart <rarrum@gmail.com>
2022-10-17 17:40:37 -0700

    Switch to qt.application so windows doesn't get an extra console.

--- xmake.lua
+++ xmake.lua
@@ -6,8 +6,8 @@ end
 
 target("EasyAutoTracker")
     set_languages("cxx20")
---    add_rules("qt.application")
-    add_rules("qt.console")
+    add_rules("qt.application")
+--    add_rules("qt.console")
     add_headerfiles("src/**.h")
     add_files("src/**.cpp")
     add_frameworks("QtWidgets", "QtGui")
